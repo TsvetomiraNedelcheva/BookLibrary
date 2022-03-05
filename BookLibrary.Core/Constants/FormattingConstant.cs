6fbd5e261694c08916a8ec6ba924cb4f0d94f69b
Tsvetomira <Tsvetomira@LAPTOP-0PH0H4UC>
2022-03-05 15:18:37 +0200

    Add project files.

---
+++ BookLibrary.Core/Constants/FormattingConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Core.Constants
+{
+    public class FormattingConstant
+    {
+        public const string NormalDateFormat = "dd.MM.yyyy";
+    }
+}
