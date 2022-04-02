46f4e72609fd1d237d7ddaa762a3f9c38821e87a
tsvetomiranedelcheva <cvetomiranedelcheva@gmail.com>
2022-04-02 18:32:25 +0300

    Move service models to a folder

---
+++ BookLibrary.Core/Services/ServiceModels/BookFormServiceModel.cs
@@ -0,0 +1,34 @@
+﻿using BookLibrary.Infrastructure.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using static BookLibrary.Infrastructure.Data.DataConstants;
+
+namespace BookLibrary.Core.Services.ServiceModels
+{
+    public class BookFormServiceModel
+    {
+        [Required]
+        [StringLength(MaxBookTitleLength, MinimumLength = MinBookTitleLength,
+           ErrorMessage = "The field must be between {2} and {1} characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(MaxBookDescriptionLength, MinimumLength = MinBookDescriptionLength,
+            ErrorMessage = "The field must be between {2} and {1} characters.")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(MinPagesValue, MaxPagesValue, ErrorMessage = "The value must be between {1} and {2}.")]
+        public int Pages { get; set; }
+
+        [Required]
+        [Display(Name = "Image Link")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(MaxPublisherNameLength, MinimumLength = MinPublisherNameLength,
+            ErrorMessage = "The field must be between {2} and {1} characters.")]
+        public string Publisher { get; set; }
+        public IEnumerable<AuthorImagesServiceModel> Authors { get; set; }
+        public ICollection<GenreType> Genres { get; set; }
+    }
+}
